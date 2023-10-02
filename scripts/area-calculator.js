aff4b5536afd5b0131b18f70249d2e5125492fa6
nomayen <nomayen.ohin@gmail.com>
2023-10-02 21:07:51 +0600

    first commit

---
+++ scripts/area-calculator.js
@@ -0,0 +1,39 @@
+function calculatorTriangleArea() {
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText);
+    console.log(base);
+
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText)
+    console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+}
+
+
+function calculatorRectangleArea() {
+    
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    console.log(width);
+
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    console.log(length);
+
+    const area = width * length;
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+
+  
+}
+
+
